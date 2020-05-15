c5e54ac7ec98598ea2ede98ee949a750fb77830d
Artem Lopatin <artlopatin@gmail.com>
2020-05-15 09:58:30 +0300

    Add button size via styled components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import {Props} from './types';
-import cn from 'classnames';
-import './index.css';
 import {StyledButton} from './styled';
 
 export const Button: React.FC<Props> = ({children, size}) => {
     return (
         <StyledButton
-            className={cn(
-                `button_size_${size}`
-            )}
+            size={size}
         >
             {children}
         </StyledButton>
--- src/components/Button/styled.tsx
+++ src/components/Button/styled.tsx
@@ -1,7 +1,30 @@
 import styled from 'styled-components';
 import {CssVar} from '../../themes/constants';
+import {ButtonSize, Props} from './types';
 
-export const StyledButton = styled.button`
+const getHeight = (size: ButtonSize) => {
+    switch (size) {
+        case ButtonSize.S:
+            return '20px';
+        case ButtonSize.M:
+            return '30px';
+        case ButtonSize.L:
+            return '40px';
+    }
+};
+
+const getBorderRadius = (size: ButtonSize) => {
+    switch (size) {
+        case ButtonSize.S:
+            return '4px';
+        case ButtonSize.M:
+            return '6px';
+        case ButtonSize.L:
+            return '8px';
+    }
+};
+
+export const StyledButton = styled.button<Props>`
     box-sizing: border-box;
     border: 2px solid;
     background: var(${CssVar.backgroundColor.main});
@@ -11,6 +34,9 @@ export const StyledButton = styled.button`
 
     color: var(${CssVar.color.main});
     border-color: #0099ff;
+    
+    height: ${p => getHeight(p.size)};
+    border-radius:${p => getBorderRadius(p.size)};
 
     :hover {
         color: #99ffff;
